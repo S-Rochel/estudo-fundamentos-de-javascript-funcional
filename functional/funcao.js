fbd82c5cd406d0604f1c1dc4bed9305b45c71d25
Sandro Rochel <sandro.rochel@gmail.com>
2022-01-11 10:49:06 -0300

    Aula 1 sobre funções

---
+++ functional/funcao.js
@@ -0,0 +1,13 @@
+// Function Declaration
+function bomDia() {
+    console.log('Bom dia!')
+}
+bomDia()
+
+
+// Function Expression
+const boaTarde = function() {
+    console.log ('Boa Tarde!')
+}
+boaTarde() // Uma Expressão de Função é quando armazena a função em uma variável, em via de regra sempre será uma função anônima e a chamada da função será pela constante.
+
