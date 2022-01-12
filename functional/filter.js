d138c0bed72db4bd2b9c144f8ea01d8fa5980ba3
Sandro Rochel <sandro.rochel@gmail.com>
2022-01-12 19:20:44 -0300

    Exercício Filter

--- functional/filter.js
+++ functional/filter.js
@@ -1,4 +1,5 @@
 // A função Filter filtra os elementos e o array resultante pode ter tamanho igual ou diferente do array original, para isso será passado uma função para a função filter.
+// Array.filter(fn())
 
 // Caso 1: 
 const numbers = [1, 2, 3, 4, 5, 6]
@@ -21,3 +22,58 @@ const studants = [
 
 const greatStudants = studants => studants.score >= 9
 console.log (studants.filter(greatStudants))
+
+
+
+console.log("x=======Exercícios=======x")
+// Exercício 1:
+const carrinho = [
+    {nome:'Caneta', qtde: 10, preco: 7.99},
+    {nome:'Impressora', qtde: 0, preco: 649.50},
+    {nome:'Caderno', qtde: 4, preco: 27.10},
+    {nome:'Lápis', qtde: 3, preco: 5.82},
+    {nome:'Tesoura', qtde: 1, preco: 19.20}
+]
+
+const qtdeMaiorQueZero = item => item.qtde > 0
+const itensValidos = carrinho.filter(qtdeMaiorQueZero)
+console.log(itensValidos)
+
+
+// Exercício 2:
+const itemNome = item => item.nome
+const itensMaior2 = item => item.qtde > 2
+
+const resultado = carrinho
+    .filter(itensMaior2)
+    .map(itemNome)
+
+console.log(resultado)    
+
+
+// Exercício 3: (construindo uma função filter)
+Array.prototype.meuFilter = function(fn){
+    const novoArray = []
+    for (let i = 0; i < this.length; i++){
+        if (fn(this[i], i, this)){
+            novoArray.push(this[i])
+        }
+    }
+    return novoArray
+}
+const carrinho2 = [
+    {nome:'Caneta', qtde: 10, preco: 7.99},
+    {nome:'Impressora', qtde: 0, preco: 649.50},
+    {nome:'Caderno', qtde: 4, preco: 27.10},
+    {nome:'Lápis', qtde: 3, preco: 5.82},
+    {nome:'Tesoura', qtde: 1, preco: 19.20}
+]
+const itemNome2 = item => item.nome
+const itensMaior = item => item.qtde > 2
+
+const resultado2 = carrinho2
+    .meuFilter(itensMaior)
+    .map(itemNome2)
+
+console.log(`O resultado de meuFilter é: ${resultado2}`)   
+ 
