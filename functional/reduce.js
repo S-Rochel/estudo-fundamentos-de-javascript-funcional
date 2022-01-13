7d0dcfaf9e37aeb7437cd30451af8ef0c18ac641
Sandro Rochel <sandro.rochel@gmail.com>
2022-01-12 21:03:50 -0300

    Exercicio desafio Map, filter e Rrduce

--- functional/reduce.js
+++ functional/reduce.js
@@ -1,4 +1,4 @@
-// Reduce Function
+// Reduce Function (Array.reduce(função, valor inicial))
 
 // Caso 1: 
 const nums = [1, 2, 3, 4, 5, 6]
@@ -29,3 +29,53 @@ const avg = (acumulador, el, indice, array) => {
 }
 const totalMedia = numbers3.reduce(avg)
 console.log (totalMedia)
+
+
+
+console.log("x=======Exercício=======x")
+// Exercício 1: (somar o geral de todos os itens do estoque)
+const carrinho1 = [
+    {nome: 'Caneta', qtde: 10, preco: 7.99},
+    {nome: 'Impressora', qtde: 0, preco: 649.50},
+    {nome: 'Caderno', qtde: 4, preco: 27.10},
+    {nome: 'Lápis', qtde: 3, preco: 5.82},
+    {nome: 'Tesoura', qtde: 1, preco: 19.20},
+]
+const valorTotalItem = item => item.qtde * item.preco
+const somarGeral = (acc, el) => acc + el
+
+const totalGeral = carrinho1
+    .map(valorTotalItem)
+    .reduce(somarGeral, 0)
+
+console.log(`A soma total de todos os itens do carrinho é: ${totalGeral}`)
+
+
+
+console.log("x=======Exercício Desafio========x")
+// Exercício 2: (desafio Map, Reduce, Filter)
+const carrinho2 = [
+    {nome: 'Caneta', qtde: 10, preco: 7.99, fragil: true},
+    {nome: 'Impressora', qtde: 1, preco: 649.50, fragil: true},
+    {nome: 'Caderno', qtde: 4, preco: 27.10, fragil: false},
+    {nome: 'Lápis', qtde: 3, preco: 5.82, fragil: false},
+    {nome: 'Tesoura', qtde: 1, preco: 19.20, fragil: true},
+]
+// 1º processamento: somente os itens que tem frágil = true:
+// 2º processamento: qtde * preco -> total:
+// 3º processamento: média totais:
+const media = carrinho2
+    .filter(item => item.fragil == true)
+    .map(item => item.qtde * item.preco)
+    .reduce((acc, el)=> {
+        const novaQtde = acc.qtde + 1
+        const novoTotal = acc.total + el
+        console.log(acc, el)
+        return{
+            qtde: novaQtde,
+            total: novoTotal,
+            media: novoTotal/novaQtde
+        }
+    },{qtde: 0, total: 0, media: 0})
+
+console.log(media)    
