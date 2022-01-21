9f502fb31f852f6b2336f68a4732e98250faf5d4
Sandro Rochel <sandro.rochel@gmail.com>
2022-01-21 19:23:36 -0300

    Aula Objetos

---
+++ exercicios/exercicio-objetos.js
@@ -0,0 +1,42 @@
+// Objetos JavaScript
+/*
+Em JavaScript, quase "tudo" é um objeto.
+
+Booleanos podem ser objetos (se definidos com a palavra- newchave)
+Os números podem ser objetos (se definidos com a palavra- newchave)
+Strings podem ser objetos (se definidos com a palavra- newchave)
+Datas são sempre objetos
+Matemática são sempre objetos
+Expressões regulares são sempre objetos
+Arrays são sempre objetos
+Funções são sempre objetos
+Objetos são sempre objetos
+Todos os valores JavaScript, exceto primitivos(string, number, boolean, null, undefined), são objetos.
+*/
+
+// Os valores do objeto são escritos como pares name : value (nome e valor separados por dois pontos).
+// É uma prática comum declarar objetos com a palavra-chave const.
+// Os valores nomeados, em objetos JavaScript, são chamados de propriedades .
+// Métodos são ações que podem ser executadas em objetos.
+
+
+// Criando um objeto JavaScript:
+/*
+Crie um único objeto, usando um literal de objeto.
+    exemplo: - const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+             - const person = {}
+
+Crie um único objeto, com a palavra-chave new Object().
+    exemplo: - const person = new Object();
+               person.firstName = "John";
+               person.lastName = "Doe";
+               person.age = 50;
+               person.eyeColor = "blue";
+
+Defina um construtor de objeto e, em seguida, crie objetos do tipo construído.
+    
+Crie um objeto usando Object.create().
+*/
+
+
+
