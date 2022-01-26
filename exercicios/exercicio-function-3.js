4ce971d8b7cd3127d8882c57881c9b096ab1e236
Sandro Rochel <sandro.rochel@gmail.com>
2022-01-26 09:57:05 -0300

    Exercício método apply()

--- exercicios/exercicio-function-3.js
+++ exercicios/exercicio-function-3.js
@@ -41,4 +41,58 @@
 
 
 
-// 
+// apply() - Com este método você pode escrever um método que pode ser usado em diferentes objetos, ele é semelhante ao método call().
+/*
+O método apply() é muito útil se você quiser usar um array em vez de uma lista de argumentos.
+*/
+// Exemplo:
+{
+    const person = {
+        fullName: function() {
+          return this.firstName + " " + this.lastName;
+        }
+      }
+      
+      const person1 = {
+        firstName: "Mary",
+        lastName: "Doe"
+      }
+      
+      // Isso vai retornar "Mary Doe":
+      person.fullName.apply(person1);
+}
+
+
+// O método apply() aceita argumentos em um array:
+// Exemplo:
+{
+    const person = {
+        fullName: function(city, country) {
+          return this.firstName + " " + this.lastName + "," + city + "," + country;
+        }
+      }
+      
+      const person1 = {
+        firstName:"John",
+        lastName: "Doe"
+      }
+      
+      person.fullName.apply(person1, ["Oslo", "Norway"]);
+}
+
+
+// Usando o método apply() para encontrar o maior número em uma lista de números, juntamente com o método Math.max().
+// Como os arrays JavaScript não têm um método max(), podemos usar:
+{
+    Math.max.apply(null, [1,2,3]); // Isso vai retornar 3
+}
+
+
+
+
+// Diferença entre call() e apply().
+/*
+    O call() método recebe argumentos separadamente .
+
+    O apply() método recebe argumentos como um array .
+*/
